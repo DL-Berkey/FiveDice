87089ca6bfaeea2e52486e194a511373b84077e7
DL-Berkey <ruyria@naver.com>
2023-10-11 18:58:50 +0900

    Adds korean fonts

--- app/src/styles/GlobalStyle.tsx
+++ app/src/styles/GlobalStyle.tsx
@@ -1,8 +1,15 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+    @font-face {
+        font-family: 'Pretendard-Regular';
+        src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+        font-weight: 300;
+        font-style: normal;
+    }
+
     :root {
-        font-family: 'Poppins', sans-serif;
+        font-family: 'Poppins', "Pretendard-Regular",sans-serif;
 
         --navy: #053B50;
         --blue: #82A0D8;

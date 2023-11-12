abc74a770588033db7829ff368e54a9976ef8ea8
DL-Berkey <ruyria@naver.com>
2023-11-13 01:04:13 +0900

    Changes Layout style and minor things

--- app/src/components/layout/Layout.tsx
+++ app/src/components/layout/Layout.tsx
@@ -1,9 +1,8 @@
+import { Outlet } from "react-router-dom";
 import styled from "styled-components";
 
 import Header from "./Header";
 import Content from "./Content";
-import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
 
 const Layout = () => {
     return (
@@ -13,17 +12,17 @@ const Layout = () => {
                 <Content>
                     <Outlet />
                 </Content>
-                <Footer />
             </Wrapper>
         </>
     );
 };
 
 const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+
     width: 1920px;
     height: 929px;
-
-    padding: 0 6%;
 `;
 
 export default Layout;

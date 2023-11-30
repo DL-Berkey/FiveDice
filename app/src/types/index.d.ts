5dffebdeef91430c97bbd23a807bfe32427476e9
DL-Berkey <ruyria@naver.com>
2023-11-30 23:37:28 +0900

    Moves Database type to index.d.ts

--- app/src/types/index.d.ts
+++ app/src/types/index.d.ts
@@ -1,3 +1,51 @@
+type Json =
+    | string
+    | number
+    | boolean
+    | null
+    | { [key: string]: Json | undefined }
+    | Json[];
+
+interface Database {
+    public: {
+        Tables: {
+            user: {
+                Row: {
+                    created_at: string;
+                    email: string;
+                    id: number;
+                    nickname: string;
+                };
+                Insert: {
+                    created_at?: string;
+                    email?: string;
+                    id?: number;
+                    nickname?: string;
+                };
+                Update: {
+                    created_at?: string;
+                    email?: string;
+                    id?: number;
+                    nickname?: string;
+                };
+                Relationships: [];
+            };
+        };
+        Views: {
+            [_ in never]: never;
+        };
+        Functions: {
+            [_ in never]: never;
+        };
+        Enums: {
+            [_ in never]: never;
+        };
+        CompositeTypes: {
+            [_ in never]: never;
+        };
+    };
+}
+
 type GameMode = "Solo" | "Rankup" | "Normal" | "Extreme";
 
 type GameResult = "win" | "defeat" | "draw";

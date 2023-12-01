3d8f83faac7bf88090423ba047e470c907f41362
DL-Berkey <ruyria@naver.com>
2023-12-01 16:27:04 +0900

    Adds queryclient to main.tsx

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,15 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.min.css";
+
 import GlobalStyle from "./styles/GlobalStyle";
 
 import App from "./App.tsx";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        mutations: {
+            retry: 3,
+        },
+    },
+});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
         <RecoilRoot>
             <GlobalStyle />
-            <App />
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
         </RecoilRoot>
+        <ToastContainer
+            position="top-right"
+            autoClose={5000}
+            limit={5}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss={false}
+            draggable
+            pauseOnHover
+            theme="light"
+        />
     </React.StrictMode>
 );
